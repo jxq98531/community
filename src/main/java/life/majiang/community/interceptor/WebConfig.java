d4fd033ba2882f60fa8617099f8ad9c762d1b59d
jxq98531 <839520257@qq.com>
2019-12-05 21:44:17 +0800

    add prof

---
+++ src/main/java/life/majiang/community/interceptor/WebConfig.java
@@ -0,0 +1,17 @@
+package life.majiang.community.interceptor;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @program: community
+ * @description
+ * @author: 金晓强
+ * @create: 2019-12-05 21:25
+ **/
+@Configuration
+@EnableWebMvc
+public class WebConfig  implements WebMvcConfigurer {
+
+}

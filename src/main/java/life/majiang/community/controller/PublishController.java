fd826d28e26def6f975e826dcd1feeac258b2515
jxq98531 <839520257@qq.com>
2019-12-05 22:11:52 +0800

    add Interceptor

--- src/main/java/life/majiang/community/controller/PublishController.java
+++ src/main/java/life/majiang/community/controller/PublishController.java
@@ -1,7 +1,6 @@
 package life.majiang.community.controller;
 
 import life.majiang.community.mapper.QuestionMapper;
-import life.majiang.community.mapper.UserMapper;
 import life.majiang.community.model.Question;
 import life.majiang.community.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,9 +9,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
-import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 
 /**
@@ -25,8 +22,6 @@ import javax.servlet.http.HttpServletRequest;
 public class PublishController {
 	@Autowired
 	private QuestionMapper questionMapper;
-	@Autowired
-	private UserMapper userMapper;
 
 	@GetMapping("/publish")
 	public String publish(){
@@ -58,22 +53,9 @@ public class PublishController {
 		}
 
 
-		User user = null;
+		User user = (User) request.getSession().getAttribute("user");
 
-		Cookie[] cookies = request.getCookies();
-		if (cookies != null && cookies.length != 0) {
-			for (Cookie cookie : cookies) {
-				if (cookie.getName().equals("token")) {
-					String token = cookie.getValue();
-					user = userMapper.findByToken(token);
 
-					if (user != null) {
-						request.getSession().setAttribute("user", user);
-					}
-					break;
-				}
-			}
-		}
 
 		if (user == null){
 			model.addAttribute("error","用户未登录");
--- src/main/java/life/majiang/community/interceptor/WebConfig.java
+++ src/main/java/life/majiang/community/interceptor/WebConfig.java
@@ -1,7 +1,8 @@
 package life.majiang.community.interceptor;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 /**
@@ -11,7 +12,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
  * @create: 2019-12-05 21:25
  **/
 @Configuration
-@EnableWebMvc
 public class WebConfig  implements WebMvcConfigurer {
+
+	@Autowired
+	private SessionInterceptor sessionInterceptor;
+
+	public void addInterceptors(InterceptorRegistry regisytry){
+		regisytry.addInterceptor(sessionInterceptor).addPathPatterns("/**");
+	}
 
 }
